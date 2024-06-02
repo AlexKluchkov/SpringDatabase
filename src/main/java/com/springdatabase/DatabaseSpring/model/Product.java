7e8cbf086404ba27c75ff71d654fe23eadcc7078
Alexander Kluchkov <136575524+AlexKluchkov@users.noreply.github.com>
2024-06-02 17:32:06 +0300

    add Getter and Setter.java

--- src/main/java/com/springdatabase/DatabaseSpring/model/Product.java
+++ src/main/java/com/springdatabase/DatabaseSpring/model/Product.java
@@ -6,14 +6,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
-@Data
 @Entity
 public class Product {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
+    @Getter @Setter
     private String name;
+    @Getter @Setter
     private int quantity;
+    @Getter @Setter
     private int price;
     public Product(){}
     public Product(String name_of_product, int quantity_of_products, int price)
