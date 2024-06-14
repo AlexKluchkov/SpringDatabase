38061a6e8de930cd7e7de05064406b200aa2fd46
Alexander Kluchkov <136575524+AlexKluchkov@users.noreply.github.com>
2024-06-14 20:30:02 +0300

    Update User.java

--- src/main/java/com/springdatabase/DatabaseSpring/entity/User.java
+++ src/main/java/com/springdatabase/DatabaseSpring/entity/User.java
@@ -1,9 +1,6 @@
 package com.springdatabase.DatabaseSpring.entity;
-import com.springdatabase.DatabaseSpring.entity.enumeration.userRole;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.springdatabase.DatabaseSpring.entity.enumeration.UserRole;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +11,8 @@ public class User
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
+    @Column(name = "fullName", columnDefinition = "VARCHAR(255)")
     String fullName;
+    @Column(name = "role", columnDefinition = "ENUM")
     private UserRole role;
 }
