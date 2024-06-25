dc446437ab12d381e7658867970ace20517e04e5
Alexander Kluchkov <136575524+AlexKluchkov@users.noreply.github.com>
2024-06-25 19:35:17 +0300

    Add files via upload

---
+++ src/main/java/com/springdatabase/DatabaseSpring/service/UserService.java
@@ -0,0 +1,22 @@
+package com.springdatabase.DatabaseSpring.service;
+
+import com.springdatabase.DatabaseSpring.entity.User;
+import com.springdatabase.DatabaseSpring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserService
+{
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    public void saveProduct(User user) {
+        userRepository.save(user);
+    }
+}
